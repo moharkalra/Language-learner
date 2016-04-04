590938fbddb41ff25e835d556673ac7034c95a60
Anand Tyagi <ananddtyagi@yahoo.com>
2016-04-04 08:40:27 -0400

    m

--- extension_template/content.js
+++ extension_template/content.js
@@ -1,21 +1,18 @@
 
-var d;
-chrome.storage.local.get("InitialTime", function(data) {
-d = data;
+var d, weekval, init;
+chrome.storage.sync.get("InitialTime", function(data) {
+  d = data;
   if (!data.InitialTime) {
-    chrome.storage.local.set({"InitialTime": Date.now()}, function () {});
+    chrome.storage.sync.set({"InitialTime": Date.now()}, function () {});
   }
-
+  init = data.InitialTime;
   var Omonth = 120;
   var time = Date.now() / 1000 - data.InitialTime / 1000 ;
   var day;
-
-
-
 });
 
 function translates(word) {
-  var b = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160403T022846Z.091eb269bff8d087.67f13b7f81138a13ea50f9020cda2e5029942df8&text="+word+"&lang=en-es&format=html";
+  var b = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160403T022846Z.091eb269bff8d087.67f13b7f81138a13ea50f9020cda2e5029942df8&text="+word+"&lang=en-fr&format=html";
   var a = new XMLHttpRequest();
   a.open('GET', b, false);
   //callback(a.responseText);
@@ -24,22 +21,27 @@ function translates(word) {
   var result = a.response.toString();
   //result.
   result = result.substring(result.indexOf("[") + 2, result.indexOf("]") - 1);
-  console.log(result + "kjh kjblk gpiyg liyk blh");
+  //console.log(result);
   return result;
 
 }
 
 var week = function() {
-
-      day = (Date.now() / 1000 - "InitialTime" / 1000);
-      return Math.floor(day / 60);
+      day = (Date.now() / 1000 - init / 1000);
+      /*return Math.floor(day);*/
+      return 2000;
 }
 
 
-var w = week();
+chrome.storage.sync.set({"Week":0}, function() {});
+  var words = ['hello', ' and ', ' but ', ' in ', ' if ', ' you ', ' a ' , ' the ', ' hand ',  ' part ',  ' child ',  ' eye ',  ' woman ',  ' place ',  ' work ',  ' week ',  ' case ',  ' point ',  ' government ',  ' company ',  ' number ',  ' group ',  ' problem ',  ' fact '];
+/*  chrome.storage.sync.set({"words": w['words']+[ ' be ',  ' have ',  ' do ',  ' say ',  ' get ',  ' make ',  ' go ',  ' know ',  ' take ',  ' see ',  ' come ',  ' think ',  ' look ',  ' want ',  ' give ',  ' use ']}, function () {});
+  chrome.storage.sync.set({"words": w['words']+[' tell ',  ' ask ',  ' work ',  ' seem ',  ' feel ',  ' try ',  ' leave ',  ' call ', ' good ',  ' new ',  ' first ',  ' last ',  ' long ',  ' great ',  ' little ',  ' own ']}, function () {});
+  chrome.storage.sync.set({"words": w['words']+[' old ',  ' right ',  ' big ',  ' high ',  ' different ',  ' small ',  ' large ',  ' next ',  ' early ',  ' young ',  ' important ',  ' few ',  ' public ',  ' bad ',  ' same ',  ' able ']}, function () {});
+*/
 var elements = document.getElementsByTagName('*');
 var replace = function() {
-  var words = [' hello ', ' and ', ' but ', ' in ', ' if ', ' you ', ' a ' , ' the '];
+
   for(y in words){
 
       var x = translates(words[y]);
@@ -53,20 +55,34 @@ var replace = function() {
                 var text = node.nodeValue;
                 text = text.toLowerCase();
                 var replacedText = text.replace(words[y], ' ' + x + ' ');
-
+                console.log(words[y] + " --> " + x);
                 if (replacedText !== text) {
                     element.replaceChild(document.createTextNode(replacedText), node);
                 }
-                else if(week( )>w && k<30){
+
+                chrome.storage.sync.get("Week", function(w) {
+
+                  //console.log(w);
+
+                  weekval = w["Week"];
+                  //  console.log(w["Week"]);
+
+                  //  console.log(week());
+
+                    if(week() > weekval  && k<30){
+
+                      words.push(text.toString());
+                      k++;
+                    //  console.log("SDF ASDF ASDF ASDF");
+                      if(k==30){
+                        chrome.storage.s    ync.set({"Week":week()}, function() {});
+                      }
+                    }
+                });
 
 
-                  words.push(text.toString);
-                  k++;
 
-                  if(k==30){
-                    w = week();
-                  }
-                }
+
 
             }
 
